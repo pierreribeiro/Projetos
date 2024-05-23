01a84ea0cc59defcde7990252dfd1951b68a24e8
Pierre Ribeiro <pierre.ribeiro@gmail.com>
2024-05-23 10:38:46 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Hello, World!")
+print("Jornada de dadpos")
+
+# Variaveis
+idade = 34
+nome = "Luciano"
+
+# Print
+print(idade)
+
+# Soma
+
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1, 2, 3, 4, 5]
+
+# Metodos
+lista_invertida = lista.reverse()
+print(lista_invertida)
